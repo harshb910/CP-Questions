5aa469861ed0def6bb28e5420743e0218f995d26
Harsh Badwaik <67547281+harshb910@users.noreply.github.com>
2021-12-18 13:56:22 +0530

    create file for the same

---
+++ K-diff Pairs in an Array.cpp
@@ -0,0 +1,65 @@
+// Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+
+// A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+
+// 0 <= i < j < nums.length
+// |nums[i] - nums[j]| == k
+// Notice that |val| denotes the absolute value of val.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,1,4,1,5], k = 2
+// Output: 2
+// Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+// Although we have two 1s in the input, we should only return the number of unique pairs.
+// Example 2:
+
+// Input: nums = [1,2,3,4,5], k = 1
+// Output: 4
+// Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+// Example 3:
+
+// Input: nums = [1,3,1,5,4], k = 0
+// Output: 1
+// Explanation: There is one 0-diff pair in the array, (1, 1).
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -107 <= nums[i] <= 107
+// 0 <= k <= 107
+//code:
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if(k<0)return 0;
+        if(k==0){
+            unordered_map<int,int>umap;
+            for(auto itr:nums)umap[itr]++;
+            int count=0;
+            for(auto itr:umap){
+                if(itr.second >1)count++;
+            }
+            return count;
+        }
+        set<int>s;
+        for(int i:nums){
+            s.insert(i);
+        }
+        int count=0;
+        for(int i:s){
+            if(s.find(k+i)!=s.end()){
+                count++;
+            }
+            if(s.find(i-k)!=s.end()){
+                count++;
+            }
+            
+            s.erase(i);
+        }
+        return count;
+    }
+};
